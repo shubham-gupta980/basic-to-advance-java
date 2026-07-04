f7a0fe8f6b49bdf150cba073a54cb47e3f21a766
shubham <acs@gmail.com>
2026-07-04 22:15:24 +0530

    Initial commit

---
+++ detect.java
@@ -0,0 +1,54 @@
+// import java.util.Scanner;
+// public class detect {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the marks of 1st");
+//         int marks1 = sc.nextInt();
+
+//         System.out.println("Enter the marks of 1st");
+//         int marks2 = sc.nextInt();
+
+//         System.out.println("Enter the marks of 1st");
+//         int marks3 = sc.nextInt();
+
+//         System.out.println("Enter the marks of 1st");
+//         int marks4 = sc.nextInt();
+//         int sum = marks1+marks2+marks3+marks4;
+//         float per =(sum*100)/400;
+//         System.out.println(per);
+//     }
+// }
+
+
+                        //  problem 2  ---> detect whether number entered by the user is integer or not 
+// import java.util.Scanner;
+// public class detect {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println(sc.hasNextInt());
+//     }
+// }
+
+
+                        //   problem 3 ---> incrept and decrept grade/
+// public class detect {
+//     public static void main(String[] args) {
+//         char grade = 'B';
+//         grade = (char)(grade+8);
+//         System.out.println(grade);
+//         grade = (char)(grade-2);
+//         System.out.println(grade);
+//     }
+// }
+
+                            //   problem 4 ---> TypeCasting in java
+// public class detect {
+//     public static void main(String[] args) {
+//         int a = 15;
+//         float b = a;
+//         System.out.println(b);
+//         double d = 12;
+//         int c = (int)d;
+//         System.out.println(c);
+//     }
+// }                           
