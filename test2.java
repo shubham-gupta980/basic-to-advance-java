11d429a338655463334c1df74c57d437bf9322ce
Shubham Gupta <sg7149006@gmail.com>
2026-07-08 13:57:25 +0530

    String Buffer

---
+++ test2.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class test2
+{
+    public static void main(String args[]){
+    BufferedReader obj = new BufferedReader( new InputStreamReader(System.in) );
+   System.out.println("Enter a number ");
+   int num1 = obj.read();
+    }
+}
